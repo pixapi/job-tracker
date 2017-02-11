929e8bdc62ddedd1a759c6c5d6aefd86b49118df
Amara Lovato <amaracuenca@gmail.com>
2017-02-11 00:26:01 -0700

    adds start of create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,25 @@
 class CommentsController < ApplicationController
-  def create
-    @comment = Comment.new(comment_params)
-    @comment.job_id = params[:job_id]
 
-    @comment.save
-
-    redirect_to company_job_path(@comment.job)
+  def new
+    @job = Job.find(params[:job_id])
+    @company = Company.find(params[:company_id])
+    @comment = Comment.new
+    @comment.job_id = @job.id
+    # byebug
   end
 
-  private
-
-  def comment_params
-    params.require(:comment).permit(:author_name, :body)
-  end
+  # def create
+  #   @comment = Comment.new(comment_params)
+  #   @comment.job_id = params[:job_id]
+  #
+  #   @comment.save
+  #
+  #   redirect_to company_job_path(@comment.job)
+  # end
+  #
+  # private
+  #
+  # def comment_params
+  #   params.require(:comment).permit(:author_name, :body)
+  # end
 end
