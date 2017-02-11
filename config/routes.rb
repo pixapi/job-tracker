c54d51eb5f1e213b4249dde3874b4fcade2b8e9c
Amara Lovato <amaracuenca@gmail.com>
2017-02-11 12:53:37 -0700

    adds contacts migration, resources, controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
   resources :companies do
+    resources :contacts
     resources :jobs do
       resources :comments
     end
   end
 
   resources :categories
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
